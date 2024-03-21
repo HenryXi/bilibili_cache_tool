75ab4534a584a94aea0e582668959eef90bd97a6
yong <xxy668@foxmail.com>
2024-03-21 14:55:52 +0800

    auto

--- src/main/java/org/example/VideoInfo.java
+++ src/main/java/org/example/VideoInfo.java
@@ -65,6 +65,8 @@ public class VideoInfo {
                 .replaceAll("\\?", "_")
                 .replaceAll("\\*","_")
                 .replaceAll("\\?", "_")
+                .replaceAll("$", "_")
+                .replaceAll("@", "_")
                 .replaceAll("`","_")
                 .replaceAll("/", "_");
     }
